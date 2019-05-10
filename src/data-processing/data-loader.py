61cc462fb3746d7bf95fcf832349a9a92d1940ca
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-10 19:57:46 +0000

    changed directory structure

---
+++ src/data-processing/data-loader.py
@@ -0,0 +1,9 @@
+## data-loader
+## functions that fetch dataset from internet for use in model training
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def fetch_batch(base_path):
+    
