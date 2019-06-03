dbdc59a813f228475b9f6ae4e09a820dfc62d45e
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-03 23:51:51 +0000

    Added code for unet

--- src/scripts/train_gan.py
+++ src/scripts/train_gan.py
@@ -23,51 +23,20 @@ class Flatten(nn.Module):
         N, C, H, W = x.shape
         return x.view(N, -1)  
 
-# class Generator(nn.Module):
-#     def __init__(self, img_shape, channel_1=32, channel_2=64, channel_3=32):
-#         super().__init__()
-        
-#         in_channel, H, W = img_shape
-        
-#         self.conv1 = nn.Conv2d(in_channel, channel_1, 5, padding=2)    
-#         self.batchNorm1 = nn.BatchNorm2d(channel_1)
-#         self.conv2 = nn.Conv2d(channel_1, channel_2, 3, padding=1)
-#         self.batchNorm2 = nn.BatchNorm2d(channel_2)
-
-#         self.unconv1 = nn.ConvTranspose2d(channel_2, channel_3, 5, stride=2, padding=1)
-#         self.batchNorm3 = nn.BatchNorm2d(channel_3)
-#         self.unconv2 = nn.ConvTranspose2d(channel_3, 3, 3, stride=2, padding=0)
-        
-#         self.relu = nn.ReLU()
-#         self.maxPool = nn.MaxPool2d(2, 2)
-
-#         nn.init.kaiming_normal_(self.conv1.weight)
-#         nn.init.kaiming_normal_(self.conv2.weight)
-#         nn.init.kaiming_normal_(self.unconv1.weight)
-#         nn.init.kaiming_normal_(self.unconv2.weight)
-        
-#     def forward(self, x):
-#         x = self.maxPool(self.batchNorm1(self.relu(self.conv1(x))))
-#         x = self.maxPool(self.batchNorm2(self.relu(self.conv2(x))))
-#         x = self.batchNorm3(self.relu(self.unconv1(x)))
-#         out_img = self.relu(self.unconv2(x))
-        
-#         return out_img
-
 class Generator(nn.Module):
-    def __init__(self, img_shape, channel_1=32, channel_2=16, channel_3=9, channel_4=5):
+    def __init__(self, img_shape, channel_1=20, channel_2=16, channel_3=8, channel_4=6):
         super().__init__()
         
         in_channel, H, W = img_shape
         
         self.conv1 = nn.Conv2d(in_channel, channel_1, 3, padding=1)    
-#         self.batchNorm1 = nn.BatchNorm2d(channel_1)
+        self.batchNorm1 = nn.BatchNorm2d(channel_1)
         self.conv2 = nn.Conv2d(channel_1, channel_2, 3, padding=1)
-#         self.batchNorm2 = nn.BatchNorm2d(channel_2)
+        self.batchNorm2 = nn.BatchNorm2d(channel_2)
         self.conv3 = nn.Conv2d(channel_2, channel_3, 3, padding=1)
-#         self.batchNorm3 = nn.BatchNorm2d(channel_3)
+        self.batchNorm3 = nn.BatchNorm2d(channel_3)
         self.conv4 = nn.Conv2d(channel_3, channel_4, 3, padding=1)
-#         self.batchNorm4 = nn.BatchNorm2d(channel_4)
+        self.batchNorm4 = nn.BatchNorm2d(channel_4)
         self.conv5 = nn.Conv2d(channel_4, in_channel, 3, padding=1)
         
         self.relu = nn.ReLU()
@@ -79,17 +48,17 @@ class Generator(nn.Module):
         nn.init.kaiming_normal_(self.conv5.weight)
         
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = self.relu(self.conv4(x))
+        x = self.batchNorm1(self.relu(self.conv1(x)))
+        x = self.batchNorm2(self.relu(self.conv2(x)))
+        x = self.batchNorm3(self.relu(self.conv3(x)))
+        x = self.batchNorm4(self.relu(self.conv4(x)))
         x = self.relu(self.conv5(x))
         
         return x
         
         
 class Discriminator(nn.Module):
-    def __init__(self, img_shape, channel_1=32, channel_2=32, channel_3=16, hidden_size=200):
+    def __init__(self, img_shape, channel_1=16, channel_2=16, channel_3=8, hidden_size=200):
         super().__init__()
 
         in_channel, H, W = img_shape
@@ -190,22 +159,24 @@ def discriminator_loss(true_scores, fake_scores):
 '''
 Initialization 
 '''
-num_epochs = 3
+num_epochs = 1
 batch_size = 100
 channels = 3
 size = 200 
 img_shape = (channels, size, size)
-lr = 1e-3
+lr = 2e-4
 b1 = .5
 b2 = .999
-sample_interval = 50
-save_interval = 5
+sample_interval = 25
+save_interval = 25
 validation_rate = 1   #Test on validation set once an epoch
 
 input_size = img_shape[0] * img_shape[1] * img_shape[2]
    
 if torch.cuda.is_available(): dtype = torch.cuda.FloatTensor
 else: dtype = torch.FloatTensor
+    
+pixel_loss = torch.nn.MSELoss().type(dtype)
 
 #Models
 generator = Generator(img_shape).type(dtype)
@@ -235,7 +206,7 @@ def save_losses(losses, path):
         for item in losses:
             f.write("{}\n".format(item))
 
-def train_batches(epoch, generator, discriminator, dataloader, train=True, save=True):
+def train_batches(epoch, generator, discriminator, dataloader, it=0, train=True, save=True):
     losses = []
     #Loop through batch
     for i, (imgs, tgts) in enumerate(dataloader):
@@ -262,34 +233,33 @@ def train_batches(epoch, generator, discriminator, dataloader, train=True, save=
             d_loss.backward()
             d_optimizer.step()
 
-        iteration = epoch * len(dataloader) + i
+        iteration = it + epoch * len(dataloader) + i
         pathval = "train" if train else "val"
         
-        print(pathval + " [Epoch %d/%d] [Batch %d/%d] [Iter %d] [G_Loss %f] [D_Loss %f]"
-        % (epoch, num_epochs, i, len(dataloader), iteration, g_loss, d_loss))
+        p_loss = pixel_loss(gen_imgs, tgts)
+        
+        print(pathval + " [Epoch %d/%d] [Batch %d/%d] [Iter %d] [G_Loss %f] [D_Loss %f] [P_Loss %f] "
+        % (epoch, num_epochs, i, len(dataloader), iteration, g_loss, d_loss, p_loss))
 
         if iteration % sample_interval == 0:
             losses.append((g_loss.item(), d_loss.item(), iteration))
+            save_losses(losses, "../../outputs/gan/" + pathval + "_losses.txt")
             save_image(imgs.data[:4], "../../outputs/gan/%d_" % iteration + pathval + "_input.png", nrow=2)
             save_image(gen_imgs.data[:4], "../../outputs/gan/%d_" % iteration + pathval + "_output.png", nrow=2)
             save_image(tgts.data[:4], "../../outputs/gan/%d_" % iteration + pathval + "_target.png", nrow=2)
         if save and (iteration % save_interval == 0): 
             torch.save(generator, "../../outputs/gan/generator.pt")
             torch.save(discriminator, "../../outputs/gan/discriminator.pt")
-            
-    return losses
 
-def train_model(generator, discriminator, save=True):
+def train_model(generator, discriminator, save=True, it=0):
     
     train_losses = []
     val_losses = []
     for epoch in range(num_epochs):
-        train_losses += train_batches(epoch, generator, discriminator, train_dataloader)
-        save_losses(train_losses, "../../outputs/gan/train_losses.txt")
+        train_losses += train_batches(epoch, generator, discriminator, train_dataloader, it)
 
         if epoch % validation_rate == 0:
-            val_losses += train_batches(epoch, generator, discriminator, val_dataloader, train=False, save=False)
-            save_losses(val_losses, "../../outputs/gan/val_losses.txt")
+            val_losses += train_batches(epoch, generator, discriminator, val_dataloader, it, train=False, save=False)
             
             
             
