a961a539f9d4cbd6e590503629d24461ba6da3e8
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-05 04:22:30 +0000

    Updated utils

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -20,28 +20,34 @@ import torch.nn.functional as F
 if torch.cuda.is_available(): dtype = torch.cuda.FloatTensor
 else: dtype = torch.FloatTensor
 
-def plot_losses(path, title, save_path):
+def plot_losses(path, title, save_path, batch_size=100):
     with open(path) as f:
         losses = f.readlines()
         losses = [line.strip()[1:-1] for line in losses]
         losses = np.array([line.split(',')[:2] for line in losses])
 
-        iters = [int(i) for i in losses[:,0]]
+        iters = [int(i) * batch_size for i in losses[:,0]]
         losses = [float(i) for i in losses[:,1]]
         
         plt.plot(iters, losses, linewidth=2.0)
         plt.title(title)
-        plt.xlabel('Iteration')
-        plt.ylabel('Reconstruction Loss')
+        plt.xlabel('Images Seen')
+        plt.ylabel('Reconstruction Error')
         plt.savefig(save_path)
-        
-def generate_imgs(model_path, save_path, split='val'):
+
+def save_losses(losses, path):
+    with open(path, 'a+') as f:
+        for item in losses:
+            f.write("{}\n".format(item))    
+
+def generate_imgs(model_path, save_path, split='test'):
     
     data = BlurDataset.from_single_dataset('../../data/coco')
-    dataloader = data.random_loader(split=split, batch_size=1)
+    dataloader = data.loader(split=split, batch_size=25)
                   
     model = torch.load(model_path)
     model.eval()
+    
     
     for i, (imgs, tgts) in enumerate(dataloader):
 
@@ -50,10 +56,13 @@ def generate_imgs(model_path, save_path, split='val'):
         tgts = tgts.type(dtype)
 
         gen_imgs = model(imgs)
-
-        save_image(imgs.data[:1], save_path +"_input.png", nrow=1)
-        save_image(gen_imgs.data[:1], save_path +"_output.png", nrow=1)
-        save_image(tgts.data[:1], save_path +"_target.png", nrow=1)
+        
+        recon_loss = F.mse_loss(gen_imgs, tgts)
+        save_losses([recon_loss], save_path+"test_loss.txt")
+        
+        save_image(imgs.data[:25], save_path + split +"_input.png", nrow=5)
+        save_image(gen_imgs.data[:25], save_path +split +"_output.png", nrow=5)
+        save_image(tgts.data[:25], save_path +split +"_target.png", nrow=5)
         
         break
 
